50f3a61e396e4894355771e98e45b337ab7933f6
Brianee445 <edewesley13@gmail.com>
2026-08-04 23:07:06 +0100

    Update import path for utility functions

--- frontend/app/components/ui/Input.tsx
+++ frontend/app/components/ui/Input.tsx
@@ -1,4 +1,4 @@
-import { cn } from "@/lib/utils";
+import { cn } from "@/app/lib/utils";
 import { forwardRef, InputHTMLAttributes } from "react";
 
 const Input = forwardRef<HTMLInputElement, InputHTMLAttributes<HTMLInputElement>>(
