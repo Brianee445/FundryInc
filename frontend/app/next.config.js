edda194f521ea81a5fa4d3d4f5e1309f1a99888b
Brianee445 <edewesley13@gmail.com>
2026-08-04 22:00:17 +0100

    Create next.config.js

---
+++ frontend/app/next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [],
+  },
+  // Ensure app directory is used
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
