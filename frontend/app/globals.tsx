de5c3c83899cfcf55b4423032b4e0367e1571ac7
Brianee445 <edewesley13@gmail.com>
2026-08-04 19:38:12 +0100

    Create globals.tsx

---
+++ frontend/app/globals.tsx
@@ -0,0 +1,16 @@
+@import url('https://api.fontshare.com/v2/css?f[]=satoshi@400,500,600,700&display=swap');
+
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+body {
+  background-color: #09090B;
+  color: #FFFFFF;
+  -webkit-font-smoothing: antialiased;
+}
+
+/* Smooth scroll */
+html {
+  scroll-behavior: smooth;
+}
