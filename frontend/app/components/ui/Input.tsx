264bbafaf579e6540ec85ef83d8ed9c7c4820d25
Brianee445 <edewesley13@gmail.com>
2026-08-04 19:32:50 +0100

    Create Input.tsx

---
+++ frontend/app/components/ui/Input.tsx
@@ -0,0 +1,20 @@
+import { cn } from "@/lib/utils";
+import { forwardRef, InputHTMLAttributes } from "react";
+
+const Input = forwardRef<HTMLInputElement, InputHTMLAttributes<HTMLInputElement>>(
+  ({ className, ...props }, ref) => {
+    return (
+      <input
+        className={cn(
+          "w-full bg-secondaryBg border border-borderColor rounded-input px-5 py-4 text-primaryText placeholder-secondaryText focus:border-primaryBlue focus:ring-1 focus:ring-primaryBlue outline-none transition",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+
+Input.displayName = "Input";
+export { Input };
